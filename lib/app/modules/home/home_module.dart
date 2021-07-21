3e06b1cacf901e23f7a8a398b8e6a91915a1710f
00653000170 <00653000170@poupex.com.br>
2021-07-20 22:02:42 -0300

    criado novos modules

--- lib/app/modules/home/home_module.dart
+++ lib/app/modules/home/home_module.dart
@@ -1,13 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import '../home/home_store.dart'; 
 
 import 'home_page.dart';
  
 class HomeModule extends Module {
   @override
   final List<Bind> binds = [
-  Bind.lazySingleton((i) => HomeStore(i.get<FirebaseAuth>())),
  ];
 
  @override
--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,34 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:mobx/mobx.dart';
-import 'home_store.dart';
+
 
 class HomePage extends StatefulWidget {
-  final String title;
-  const HomePage({Key? key, this.title = "Home"}) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends ModularState<HomePage, HomeStore> {
+class _HomePageState extends State<HomePage> {
 
-  late final ReactionDisposer _disposer;
-
-  @override
-  void initState(){
-    super.initState();
-
-    _disposer = when((_) => store.user == null , () => Modular.to.pushReplacementNamed('/login'));
-
-  }
-
-  @override
-  void dispose() {
-    _disposer();
-    super.dispose();
-  }
 
 
   @override
@@ -36,16 +18,22 @@ class _HomePageState extends ModularState<HomePage, HomeStore> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.add_box_outlined)),
+          IconButton(onPressed: (){}, icon: Icon(Icons.favorite_border_outlined)),
+          IconButton(onPressed: (){}, icon: Icon(Icons.chat_bubble_outline_rounded)),
+        ],
       ),
-      body: Observer(
-        builder: (context) => Text('Você está logado ${store.user?.displayName}'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          store.logout();
-        },
-        child: Icon(Icons.logout),
-      ),
+        body: RouterOutlet(),
+        bottomNavigationBar: BottomNavigationBar(
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Buscar'),
+            BottomNavigationBarItem(icon: Icon(Icons.person_outline), label: 'Perfil'),
+          ],
+        )
     );
   }
 }
